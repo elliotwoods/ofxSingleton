3af7ff04ff2fcb71a11c0960e8dfe32f043f4165
Elliot Woods <elliot-web@kimchiandchips.com>
2015-08-07 13:26:04 +0900

    master store code change

--- src/ofxSingleton/Singleton.h
+++ src/ofxSingleton/Singleton.h
@@ -15,7 +15,6 @@ namespace ofxSingleton {
 		///The Store keeps the singleton, we always have an instance of it
 		///mostly because it is cheaper to always 
 		class SingletonStore : public BaseStore {
-			std::shared_ptr<ClassType> instance;
 		public:
 			SingletonStore() {
 				auto & singletonRegister = Register::X();
@@ -29,22 +28,19 @@ namespace ofxSingleton {
 
 			///Always returns a valid pointer (i.e. create on first call)
 			std::shared_ptr<ClassType> getInstance() {	
-				if (!this->instance) {
-					//no instance yet here
-					if (this->hasMaster()) {
-						//we have a master application, let's ask that for a valid instance
-						auto masterStore = dynamic_cast<SingletonStore*>(this->masterStoreUntyped);
-						if (masterStore) {
-							//this may cause an early initialisation on the master store, but seems like a safe strategy
-							this->instance = masterStore->getInstance();
-						}
-					} else {
-						//we have no master, we will create the instance
-						this->instance = std::make_shared<ClassType>();
-					}
+				//if we have a host application, use the singleton from that
+				if (this->hasMaster()) {
+					auto masterStore = static_cast<SingletonStore*>(this->masterStoreUntyped);
+					return masterStore->getInstance();
+				}
+				//otherwise use local
+				if(!this->instance) {
+					this->instance = std::make_shared<ClassType>();
 				}
 				return this->instance;
 			}
+		protected:
+			std::shared_ptr<ClassType> instance;
 		};
 
 		static ClassType & X() {
